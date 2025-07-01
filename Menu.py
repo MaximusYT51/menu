1a557e5a1afd3e4ef86286c8e8b9da9fb0b3bf39
MaximusYT51 <88859248+MaximusYT51@users.noreply.github.com>
2025-07-01 15:05:15 -0400

    Add files via upload
    menujaja

---
+++ Menu.py
@@ -0,0 +1,12 @@
+def menu(titulo,opciones):
+    i=1
+    print(titulo)
+    print('==================')
+    for opc in opciones:
+        print(i,'.-',opc)
+        i+=1
+    print(i,'.-','Salir')
+    opcion=input('Ingrese Opción: ')
+    return opcion
+
+print(menu('Menú Calculadora',['Sumar','Restar','Multiplicar','Dividir']))
